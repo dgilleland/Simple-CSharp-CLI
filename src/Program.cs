410fe347df7f7d10f914984bae54eddaf5b6f106
Dan Gilleland <dgilleland@nait.ca>
2024-02-26 11:33:12 -0700

    Convert Program.cs from TLP to traditional Main style

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using static System.Console;
 
-WriteLine("Hello, World!");
-string terminalInput = string.Join(',', args);
-WriteLine($"A special hello to {terminalInput}");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WriteLine("Hello, World!");
+        string terminalInput = string.Join(',', args);
+        WriteLine($"A special hello to {terminalInput}");
+    }
+}
